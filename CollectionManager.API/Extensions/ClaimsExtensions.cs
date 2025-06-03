6a1b7e20d186ec08f428dfdb7c48112dc81eceb7
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-03 19:17:08 +0300

    Implemented claims extensions.

---
+++ CollectionManager.API/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+
+namespace xhunter74.CollectionManager.API.Extensions;
+
+public static class ClaimsExtensions
+{
+    
+    public static Guid UserId(this ClaimsPrincipal user)
+    {
+        if (user.Identity != null && user.Identity.IsAuthenticated)
+        {
+            var id = user.Identity.Name;
+            if (!string.IsNullOrWhiteSpace(id))
+                return new Guid(id);
+            throw new InvalidDataException("UserId has not specified");
+        }
+
+        return Guid.Empty;
+    }
+
+    public static string UserName(this ClaimsPrincipal user)
+    {
+        if (user.Identity.IsAuthenticated)
+        {
+            var userName = user.Claims
+                .Where(x => x.Type.Equals("preferred_username", StringComparison.InvariantCultureIgnoreCase))
+                .Select(x => x.Value).FirstOrDefault();
+            if (!string.IsNullOrEmpty(userName))
+                return userName;
+            throw new InvalidDataException("UserName has not specified");
+        }
+
+        return string.Empty;
+    }
+}
