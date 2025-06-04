7f5f818652eefa3e2f2009d3f8ff177d6a957e77
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-04 18:32:49 +0300

    Changed settings for debugging.

--- CollectionManager.API/Settings/IdentitySettings.cs
+++ CollectionManager.API/Settings/IdentitySettings.cs
@@ -10,8 +10,9 @@ public class IdentitySettings
     public string DefaultClientId { get; set; }
     [Required(AllowEmptyStrings = false)]
     public string DefaultClientSecret { get; set; }
+    //TODO Return [Range(60, 3600, ErrorMessage = "Access token lifetime must be between 60 and 3600 seconds.")]
     [Required]
-    [Range(60, 3600, ErrorMessage = "Access token lifetime must be between 60 and 3600 seconds.")]
+    [Range(60, 10800, ErrorMessage = "Access token lifetime must be between 60 and 3600 seconds.")]
     public int AccessTokenLifetime { get; set; }
     [Required]
     [Range(3600, 10080, ErrorMessage = "Refresh token lifetime must be between 3600 and 10800 seconds.")]
