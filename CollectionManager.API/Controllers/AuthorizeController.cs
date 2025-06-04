8214856cdb517476527fdc4fe3f3d5a1a544b5bb
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-04 09:19:03 +0300

    Refactored. Created command to create token from refresh token.

--- CollectionManager.API/Controllers/AuthorizeController.cs
+++ CollectionManager.API/Controllers/AuthorizeController.cs
@@ -3,12 +3,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 using OpenIddict.Abstractions;
 using OpenIddict.Server.AspNetCore;
-using System.Security.Claims;
 using xhunter74.CollectionManager.API.Features.Authorize;
-using xhunter74.CollectionManager.API.Permissions;
 using xhunter74.CollectionManager.Data.Entity;
 
 namespace xhunter74.CollectionManager.API.Controllers;
@@ -49,87 +46,29 @@ public class AuthorizationController : Controller
         }
 
         if (oidcRequest.IsRefreshTokenGrantType())
-            return await TokensForRefreshTokenGrantType();
+        {
+            var authResult = await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+            if (authResult?.Principal == null)
+            {
+                return BadRequest(new OpenIddictResponse
+                {
+                    Error = OpenIddictConstants.Errors.InvalidGrant,
+                    ErrorDescription = "The refresh token is no longer valid."
+                });
+            }
+
+            var userId = authResult.Principal.GetClaim(OpenIddictConstants.Claims.Subject);
+            var command = new GetTokenForRefreshTokenCommand
+            {
+                UserId = userId
+            };
+            var result = await _mediatr.SendAsync(command);
+            return result;
+        }
 
         return BadRequest(new OpenIddictResponse
         {
             Error = OpenIddictConstants.Errors.UnsupportedGrantType
         });
-    }
-
-    private async Task<Microsoft.AspNetCore.Mvc.SignInResult> CreateTokenForUserAsync(ApplicationUser user)
-    {
-        var identity = new ClaimsIdentity(
-                TokenValidationParameters.DefaultAuthenticationType,
-                OpenIddictConstants.Claims.Name,
-                OpenIddictConstants.Claims.Role);
-
-        identity.AddClaim(OpenIddictConstants.Claims.Subject, user.Id.ToString(), OpenIddictConstants.Destinations.AccessToken);
-        identity.AddClaim(OpenIddictConstants.Claims.Username, user.UserName, OpenIddictConstants.Destinations.AccessToken);
-
-        var userClaims = await _userManager.GetClaimsAsync(user);
-
-        foreach (var claim in userClaims.Where(e => e.Type == AppClaimTypes.UserPermissionClaim))
-        {
-            claim.SetDestinations(OpenIddictConstants.Destinations.AccessToken, OpenIddictConstants.Destinations.IdentityToken);
-            identity.AddClaim(claim);
-        }
-
-        var claimsPrincipal = new ClaimsPrincipal(identity);
-        claimsPrincipal.SetScopes(new string[]
-        {
-                OpenIddictConstants.Scopes.Roles,
-                OpenIddictConstants.Scopes.OfflineAccess,
-                OpenIddictConstants.Scopes.Email,
-                OpenIddictConstants.Scopes.Profile,
-        });
-
-        return SignIn(claimsPrincipal, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-    }
-
-    private async Task<IActionResult> TokensForRefreshTokenGrantType()
-    {
-        var authResult = await HttpContext.AuthenticateAsync(OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
-        if (authResult?.Principal == null)
-        {
-            return BadRequest(new OpenIddictResponse
-            {
-                Error = OpenIddictConstants.Errors.InvalidGrant,
-                ErrorDescription = "The refresh token is no longer valid."
-            });
-        }
-
-        var userId = authResult.Principal.GetClaim(OpenIddictConstants.Claims.Subject);
-        if (string.IsNullOrEmpty(userId))
-        {
-            return BadRequest(new OpenIddictResponse
-            {
-                Error = OpenIddictConstants.Errors.ServerError,
-                ErrorDescription = "The token is missing a subject claim."
-            });
-        }
-
-        var user = await _userManager.FindByIdAsync(userId);
-        if (user == null)
-        {
-            return BadRequest(new OpenIddictResponse
-            {
-                Error = OpenIddictConstants.Errors.InvalidGrant,
-                ErrorDescription = "The refresh token is no longer valid."
-            });
-        }
-
-        if (!await _signInManager.CanSignInAsync(user))
-        {
-            return BadRequest(new OpenIddictResponse
-            {
-                Error = OpenIddictConstants.Errors.InvalidGrant,
-                ErrorDescription = "The user is no longer allowed to sign in."
-            });
-        }
-
-        var result = await CreateTokenForUserAsync(user);
-
-        return result;
     }
 }
---
+++ CollectionManager.API/Features/Authorize/GetTokenForRefreshTokenCommand.cs
@@ -0,0 +1,48 @@
+﻿using CQRSMediatr.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using xhunter74.CollectionManager.Data.Entity;
+using xhunter74.CollectionManager.Shared.Exceptions;
+
+namespace xhunter74.CollectionManager.API.Features.Authorize;
+
+public class GetTokenForRefreshTokenCommand : ICommand<Microsoft.AspNetCore.Mvc.SignInResult>
+{
+    public string UserId { get; set; }
+}
+
+public class GetTokenForRefreshTokenCommandHandler : BaseFeatureHandler, ICommandHandler<GetTokenForRefreshTokenCommand,
+    Microsoft.AspNetCore.Mvc.SignInResult>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly CollectionsDbContext _dbContext;
+
+    public GetTokenForRefreshTokenCommandHandler(
+        ILogger<GetTokenForPasswordCommandHandler> logger,
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager
+        ) : base(logger)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public async Task<Microsoft.AspNetCore.Mvc.SignInResult> HandleAsync(GetTokenForRefreshTokenCommand command, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(command.UserId))
+            throw new BadRequestException("The token is missing a subject claim.");
+
+        var user = await _userManager.FindByIdAsync(command.UserId);
+        if (user == null)
+            throw new BadRequestException("The refresh token is no longer valid.");
+
+
+        if (!await _signInManager.CanSignInAsync(user))
+            throw new BadRequestException("The user is no longer allowed to sign in.");
+
+        var authorizeUtils = new AuthorizeUtils();
+        var result = await authorizeUtils.CreateTokenForUserAsync(_userManager, user);
+
+        return result;
+    }
+}
