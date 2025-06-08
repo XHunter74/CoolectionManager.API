df9a40f5fdb10960b37cdc6d6f7bb6095721d7a3
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-08 22:58:31 +0300

    Added test controller.

---
+++ CollectionManager.API/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using xhunter74.CollectionManager.Shared.Services.Interfaces;
+
+namespace xhunter74.CollectionManager.API.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    private readonly ILogger<TestController> _logger;
+    private readonly IEmailService _emailService;
+
+    public TestController(
+        ILogger<TestController> logger,
+        IEmailService emailService
+        )
+    {
+        _logger = logger;
+        _emailService = emailService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Test()
+    {
+        await _emailService.SendEmailAsync("xhunter74@gmail.com", "Test email", "<p>Test</p>");
+        return Ok();
+    }
+}
