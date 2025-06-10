fdbc7956c15d4cac0f94b675a5edfefc32ed9628
serhiy.krasovskyy <xhunter74@gmail.com>
2025-06-10 12:38:58 +0300

    Added method to get field types.

---
+++ CollectionManager.API/Models/FieldTypeDto.cs
@@ -0,0 +1,7 @@
+﻿namespace xhunter74.CollectionManager.API.Models;
+
+public class FieldTypeDto
+{
+    public string Name { get; set; }
+    public int Value { get; set; }
+}
--- CollectionManager.Data/Entity/FieldTypes.cs
+++ CollectionManager.Data/Entity/FieldTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace xhunter74.CollectionManager.Data.Entity;
+﻿namespace xhunter74.CollectionManager.Data.Entity;
 
 public enum FieldTypes
 {
